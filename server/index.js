c18d3cf2d81cba1095a873e832c17e5ca3df3a2c
HDunlop <Dunlop.H@etoncollege.org.uk>
2020-04-24 14:52:05 +0800

    i give up for the time being...

--- server/index.js
+++ server/index.js
@@ -7,6 +7,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-//basci rundown to make socket.io server work
+//basic rundown to make socket.io server work
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
